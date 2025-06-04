cb72af904fd7143f4fe006c297101b66df898fae
Arc-lead <info@arc-lead.co.jp>
2025-06-05 06:08:59 +0900

    Fix: change port to 5174 in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// これが入っているか確認
+server: {
+  port: 5174
+}
 export default defineConfig({
   plugins: [react()],
 })
